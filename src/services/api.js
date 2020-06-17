e5b38d73cf559a0dd699b5778a103dacebbc403d
djonata-dias <diasdjonata@gmail.com>
2020-06-17 16:51:16 -0300

    1- Criadas as funções de sincronização da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,22 @@
-export async function getCategories() {
-  // implement here
+export function getCategories() {
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const categoriesAPI = await fetch(url, { mode: 'no-cors' })
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+  return categoriesAPI;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // implement here
+  let url;
+  if (categoryId && query) {
+    url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  } else if (categoryId) {
+    url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  } else {
+    url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  }
+  const seachAPI = await fetch(url, { mode: 'no-cors' })
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+  return seachAPI;
 }
