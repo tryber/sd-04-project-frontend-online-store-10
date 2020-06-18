56a9f9c91dcc1a975d638a31deacc53aa6446125
djonata-dias <diasdjonata@gmail.com>
2020-06-18 17:09:00 -0300

    4- Criei o componente que faz a requisição da API e retorna uma lista de categorias

--- src/componentes/Categories.jsx
+++ src/componentes/Categories.jsx
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import * as API from '../services/api';
+import Loading from './Loanding';
 
 class Categories extends Component {
   constructor(props) {
     super(props);
-    this.state = ({ categories: null });
-  }
-
-  componentDidMount() {
-    API.getCategories().then((data) => {
-      console.log(data);
-      return this.setState({ categories: data });
+    this.state = ({
+      categories: null,
+      loading: true,
     });
   }
 
+  componentDidMount() {
+    API.getCategories().then((data) => this.setState({ categories: data, loading: false }));
+  }
+
   render() {
-    const { categories } = this.state;
+    const { categories, loading } = this.state;
+    if (loading) { return (<Loading />); }
     return (
       <ol>
-        {categories.map(({ id, name }) => <li key={id}>{name}</li>)}
+        {categories.map(({ id, name }) => (
+          <li className="category-list">
+            <label data-testid="category" htmlFor={id}>
+              <input name="categories" id={id} value={id} type="radio" />
+              {name}
+            </label>
+          </li>
+        ))}
       </ol>
     );
   }
