1090d10a860342804ce9cb9d7e5849895082c152
djonata-dias <diasdjonata@gmail.com>
2020-06-18 17:09:52 -0300

    4- Tirei o Browser Router para não dar confilto caso a dependencia não esteja instalada

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
 import Categorie from './componentes/Categories';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <Categorie />
-        </header>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <Categorie />
+      </header>
+    </div>
   );
 }
 
