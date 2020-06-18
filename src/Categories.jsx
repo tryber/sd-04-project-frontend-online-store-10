b0cfce8cee35af405f75769ef93f86d53f0dfebb
djonata-dias <diasdjonata@gmail.com>
2020-06-17 22:32:07 -0300

    2- Criei o componente que renderiza as categorias

---
+++ src/Categories.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import * as API from './services/api';
+
+class Categories extends Component {
+  constructor(props) {
+    super(props);
+    this.state = ({ categories: null });
+  }
+
+  componentDidMount() {
+    API.getCategories().then((data) => {
+      console.log(data);
+      return this.setState({ categories: data });
+    });
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <ol>
+        {categories.map(({ id, name }) => <li key={id}>{name}</li>)}
+      </ol>
+    );
+  }
+}
+
+export default Categories;
