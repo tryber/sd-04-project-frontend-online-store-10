9db71f3c9f28c2cfe3ea2722bbe990cfee071dbd
gabriellukke <gabriellukke@outlook.com>
2020-06-19 15:00:28 -0300

    Componente SearchBar criado e estilizado

---
+++ src/componentes/SearchBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onClick } = this.props;
+    return (
+      <div className="main-search-bar">
+        <label htmlFor="search">
+          <input
+            className="input-search-bar"
+            type="text"
+            name="search"
+            value={searchText}
+          />
+          <button
+            type="button"
+            className="btn-search-bar"
+            onClick={onClick}
+          />
+        </label>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default SearchBar;
