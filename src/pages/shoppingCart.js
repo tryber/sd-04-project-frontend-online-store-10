8e9d31dfaa79635ce06b25a20cd08d4ae4494282
djonata-dias <diasdjonata@gmail.com>
2020-06-20 19:12:56 -0300

    Coloquei o botão com a imagem de seta para voltar para home page

--- src/pages/shoppingCart.js
+++ src/pages/shoppingCart.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Cart from '../componentes/Cart';
 
 function ShoppoingCart() {
   return (
     <div>
       <Cart />
+      <Link to="/"><img src="../images/seta-voltar.png" alt="imagem de seta para voltar" /></Link>
     </div>
   );
 }
