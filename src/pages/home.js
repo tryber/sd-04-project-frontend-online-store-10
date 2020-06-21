bb856207baf07cb2aea4f8932fa323e2cd4926d2
djonata-dias <diasdjonata@gmail.com>
2020-06-21 03:22:09 -0300

    adicionando componentes e elementos na pagina inicial

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './css/home.css';
+import Categories from '../componentes/Categories';
+import ItemsList from '../componentes/ItemsList';
+import SearchBar from '../componentes/SearchBar';
+import CartIcon from '../images/shopping-cart.png';
 
 function Home() {
   return (
-    <div data-testeid="home-initial-message">
-      <h2>Digite algum termo de pesquisa ou escolha uma</h2>
+    <div className="home-container">
+      <div className="nav-page">
+        <SearchBar className="searchBar" />
+        <Link to="/shopping-cart" data-testid="shopping-cart-button">
+          <img src={CartIcon} alt="icone de carrinho de compras" className="cart-icon" />
+        </Link>
+      </div>
+
+      <div className="content-container">
+        <div className="categories-container"><Categories /></div>
+        <div className="items-container">
+          <ItemsList />
+        </div>
+      </div>
+
     </div>
   );
 }
