d899b0d543a9783b9ed6721c4cc8484f2eef9a12
djonata-dias <diasdjonata@gmail.com>
2020-06-22 15:17:51 -0300

    adicionei as funções que pegam o value dos componentes e passei como props para eles

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './css/home.css';
 import Categories from '../componentes/Categories';
@@ -6,25 +6,57 @@ import ItemsList from '../componentes/ItemsList';
 import SearchBar from '../componentes/SearchBar';
 import CartIcon from '../images/shopping-cart.png';
 
-function Home() {
-  return (
-    <div className="home-container">
-      <div className="nav-page">
-        <SearchBar className="searchBar" />
-        <Link to="/shopping-cart" data-testid="shopping-cart-button">
-          <img src={CartIcon} alt="icone de carrinho de compras" className="cart-icon" />
-        </Link>
-      </div>
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      categoryId: '',
+      items: null,
+    };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onSelectedCategoryChange = this.onSelectedCategoryChange.bind(this);
+  }
+  
+  onSearchTextChange(e) {
+    const value = e.target.value;
+    this.setState({ searchText: value });
+  }
 
-      <div className="content-container">
-        <div className="categories-container"><Categories /></div>
-        <div className="items-container">
-          <ItemsList />
+  onSelectedCategoryChange(e) {
+    const value = e.target.value;
+    this.setState({ categoryId: value });
+  }
+
+  render() {
+    const { searchText, categoryId } = this.state;
+    return (
+      <div className="home-container">
+        <div className="nav-page">
+          <SearchBar
+            onSearchTextChange={this.onSearchTextChange}
+            searchText={searchText}
+            className="searchBar"
+          />
+          <Link to="/shopping-cart" data-testid="shopping-cart-button">
+            <img src={CartIcon} alt="icone de carrinho de compras" className="cart-icon" />
+          </Link>
         </div>
-      </div>
+        <div className="content-container">
+          <div className="categories-container">
+            <Categories
+              category={categoryId}
+              onSelectedCategoryChange={this.onSelectedCategoryChange}
+            />
+          </div>
+          <div className="items-container">
+            <ItemsList searchText={searchText} categoryId={categoryId} />
+          </div>
+        </div>
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default Home;
