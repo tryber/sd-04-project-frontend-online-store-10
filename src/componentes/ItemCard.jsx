cbce8dbf96d1c0f055b80446862ea188dbdcde85
djonata-dias <diasdjonata@gmail.com>
2020-06-22 15:13:38 -0300

    Criei o componente movieCard que cria um cartão com as informçãoes de cada item retornado pela pesquisa

--- src/componentes/ItemCard.jsx
+++ src/componentes/ItemCard.jsx
@@ -5,13 +5,12 @@ import { Link } from 'react-router-dom';
 class itemCard extends React.Component {
   render() {
     const { item } = this.props;
-    const { thumbnail, title, price } = item;
     return (
       <div className="item-card">
-        <img alt="item Cover" className="item-card-image" src={thumbnail} />
+        <img alt="item Cover" className="item-card-image" src={item.thumbnail} />
         <div className="item-card-body">
-          <h2 className="item-card-title">{title}</h2>
-          <p className="item-card-subtitle">{price}</p>
+          <h2 className="item-card-title">{item.title}</h2>
+          <p className="item-card-subtitle">{item.price}</p>
           <Link to="item-details">Mostrar detalhes</Link>
         </div>
       </div>
