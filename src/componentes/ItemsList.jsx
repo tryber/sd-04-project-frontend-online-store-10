9f3d54f8ecfccbc37f770b65b5325b056f78620f
djonata-dias <diasdjonata@gmail.com>
2020-06-22 15:15:41 -0300

    Adicionei a função que faz a requisição para API conforme a props passada a ela e renderiza os itemCards com os items retornados pela função

--- src/componentes/ItemsList.jsx
+++ src/componentes/ItemsList.jsx
@@ -2,32 +2,48 @@ import React from 'react';
 import ItemCard from './ItemCard';
 import * as API from '../services/api';
 
-class ItemsList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: 'empty',
-      items: null,
-    };
-  }
 
-  componentDidMount() {
-    const { categoryId, query } = this.props;
-    if (categoryId || query) {
-      API.getProductsFromCategoryAndQuery(categoryId, query).then((items) => {
-        this.setState({ items, status: 'loaded' });
+class ItemsList extends React.Component {
+constructor(props){
+  super(props);
+  this.state = {
+    items: null,
+    loading: true,
+  }
+  this.props = ({ categoryId: null, searchText: null })
+}
+
+
+
+async UNSAFE_componentWillUpdate(nextProps){
+    const { categoryId, searchText } = nextProps;
+    console.log(nextProps);
+    console.log(this.props);
+    
+    if(categoryId || searchText){
+     await API.getProductsFromCategoryAndQuery(categoryId, searchText).then((data) => {
+        const {results} = data;
+        console.log(results);
+
+         this.setState({ items: results, loading: false });
       });
     }
   }
-
   render() {
-    const { status, items } = this.state;
-    if (status === 'empty') {
-      return (<h2 data-testeid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h2>);
+    const { items, loading } = this.state;
+    if (loading || !items) {
+      console.log("teste" + items);
+      return (
+        
+        <h2 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>
+      );
     }
+
     return (
       <div className="items-list">
-        {items.map((item) => <ItemCard key={item.title} movie={item} />)}
+        {items.map((item) => <ItemCard key={item.id} item={item} />)}
       </div>
     );
   }
