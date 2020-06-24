02ac759d557e286c68867a218e933299ca871d7d
djonata-dias <diasdjonata@gmail.com>
2020-06-24 18:50:05 -0300

    criei o componente itemDetails

---
+++ src/pages/itemDetails.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Loading from '../componentes/Loanding';
+
+class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: {
+        title: '',
+        storyline: '',
+        genre: '',
+        rating: 0,
+        subtitle: '',
+        imagePath: '',
+        id: '',
+      },
+      loading: true,
+      redirect: false,
+    };
+  }
+ 
+  render() {
+    const { loading, redirect } = this.state;
+    const { movie } = this.props;
+    if (redirect) { return <Redirect to="/" />; }
+    if (loading) { return <Loading />; }
+    const { storyline, imagePath, genre, rating, title, subtitle, id } = movie;
+    return (
+      <div data-testid="movie-details">
+        <h1>{title}</h1>
+        <img alt="Movie Cover" src={`/${imagePath}`} />
+        <p>{`Subtitle: ${subtitle}`}</p>
+        <p>{`Storyline: ${storyline}`}</p>
+        <p>{`Genre: ${genre}`}</p>
+        <p>{`Rating: ${rating}`}</p>
+        <div>
+          <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+          <Link to="/">VOLTAR</Link>
+          <Link to="/" onClick={() => { movieAPI.deleteMovie(id); }}>
+            DELETAR
+            </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
+
+export default MovieDetails;
