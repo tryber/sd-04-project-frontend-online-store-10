4c73dac11c35c1a668514ec4ef535f73241bf15a
djonata-dias <diasdjonata@gmail.com>
2020-06-24 22:41:02 -0300

    Adicionando states para armazenar valor de pesquisa, key no map de itemCard e condicional de mensagem inicial

--- src/componentes/ItemsList.jsx
+++ src/componentes/ItemsList.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ItemCard from './ItemCard';
+// import * as API from '../services/api';
 
 class ItemsList extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = ({
+      item: '',
+      categoryId: '',
+      searchText: '',
+    });
+  }
 
   render() {
+    const { status } = this.state;
     const { items } = this.props;
-    if (!items) {
+    if (!items && !status) {
       return (
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
@@ -16,14 +25,10 @@ class ItemsList extends React.Component {
 
     return (
       <div className="items-list">
-        {items.map((item) => <ItemCard data-testid="product" item={item} /> )}
+        {items.map((item) => <ItemCard key={item.id} item={item} />)}
       </div>
     );
   }
-}
-
-ItemsList.defaultProps = {
-  items: '',
 }
 
 export default ItemsList;
