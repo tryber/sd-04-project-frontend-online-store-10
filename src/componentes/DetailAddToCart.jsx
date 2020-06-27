dfb6495f47826baebe96796a9dd41607067ef57b
Josias Viveiro <josiasviveiro@gmail.com>
2020-06-27 17:38:36 -0300

    DetailAddToCart criado para o requisito 9, estrutura basica

---
+++ src/componentes/DetailAddToCart.jsx
@@ -0,0 +1,37 @@
+/*
+9. Adicionar um produto ao carrinho a partir de sua tela de exibição detalhada
+
+- Poder adicionar produtos ao carrinho a partir de sua tela de exibição detalhada será um canal importante de conversões de venda.
+
+- Adicione o atributo data-testid com o valor product-detail-add-to-cart no elemento que possui a ação de adicionar o produto ao carrinho de compras.
+
+Na exibição detalhada do produto, permitir adicionar o produto ao carrinho e alterar sua quantidade (botões (-) e (+)). A quantidade não pode ser negativa.
+
+- Card 09
+DetailAddToCart
+*/
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class DetailAddToCart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+// DetailAddToCart.propTypes = {
+// };
+
+export default DetailAddToCart;
+
