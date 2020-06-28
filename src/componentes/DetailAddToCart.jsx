6cf89c83227d9162e5021991fa1169eb7c93ec6d
Josias Viveiro <josiasviveiro@gmail.com>
2020-06-27 22:11:10 -0300

    Botoes adicionados, aumenta e diminui a quantidade do produto

--- src/componentes/DetailAddToCart.jsx
+++ src/componentes/DetailAddToCart.jsx
@@ -18,9 +18,26 @@ class DetailAddToCart extends React.Component {
     super(props);
 
     this.state = {
-      quantity: 0,
+      quantity: 1,
     };
 
+    this.plusQuantity = this.plusQuantity.bind(this);
+    this.minusQuantity = this.minusQuantity.bind(this);
+  }
+  plusQuantity() {
+    this.setState((state) => {
+      return { quantity: state.quantity + 1 };
+    });
+  }
+
+  minusQuantity() {
+    this.setState((state) => {
+      if (state.quantity <= 0 ) {
+        return { quantity: 0 };
+      } else {
+      return { quantity: state.quantity - 1 };
+      }
+    });
   }
 
   render() {
@@ -29,11 +46,13 @@ class DetailAddToCart extends React.Component {
       <div>
         Quantidade
         <div>
-          <button>-</button>
+          <button onClick={this.minusQuantity}>-</button>
           <p>{quantity}</p>
-          <button>+</button>
+          <button onClick={this.plusQuantity}>+</button>
         </div>
-        <button>Adicionar ao Carrinho</button>
+        <button className="product-detail-add-to-cart">
+          Adicionar ao Carrinho
+        </button>
       </div>
     );
   }
